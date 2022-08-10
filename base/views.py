e24fc74bda010ec46bee5785a4fb7243cc59c024
Jakub Rajs <jakubmarcin.rajs@gmail.com>
2022-08-10 22:46:54 +0200

    Added the option to update and delete tasks

--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import  ListView
 from django.views.generic.detail import  DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from . models import Task
 # from django.http import HttpResponse
 
@@ -13,3 +16,17 @@ class TaskDetail(DetailView):
     context_object_name = 'task'
     template_name = 'base/task_det2.html'
 
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
