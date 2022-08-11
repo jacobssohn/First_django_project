ffb8af08b4e4b559c4dc0934e2152ab238640aec
bjsmn1 <jakub.rajs@aptiv.com>
2022-08-11 17:02:37 +0200

    Added restricted user view to view list, create and update tasks

--- base/views.py
+++ base/views.py
@@ -24,7 +24,13 @@ class CustomLoginView(LoginView):
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
-    context_object_name = 'zadania'
+    context_object_name = 'tasks'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
@@ -33,13 +39,22 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    # fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskUpdate, self).form_valid(form)
 
 class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
